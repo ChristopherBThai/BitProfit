0878043803360ea113c6ff26b4018a226f22cb4b
Chris <ChristopherBThai@gmail.com>
2018-01-03 23:50:20 -0800

    Top right button now brings up add currency layout

--- app/src/main/java/com/bitprofit/mono/bitprofit/MainActivity.java
+++ app/src/main/java/com/bitprofit/mono/bitprofit/MainActivity.java
@@ -1,15 +1,21 @@
 package com.bitprofit.mono.bitprofit;
 
+import android.app.Activity;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.support.v7.widget.RecyclerView;
 
-import com.bitprofit.mono.bitprofit.async.FetchData;
 import com.bitprofit.mono.bitprofit.async.ReadJson;
 import com.bitprofit.mono.bitprofit.async.WriteJson;
 import com.bitprofit.mono.bitprofit.variables.Var;
@@ -21,6 +27,8 @@ public class MainActivity extends AppCompatActivity{
 	public static TextView total, totalProfit;
 	private static RecyclerView recycle;
 	private static RVAdapter adapter;
+	RelativeLayout adder;
+	private static ViewGroup decor;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
@@ -44,6 +52,24 @@ public class MainActivity extends AppCompatActivity{
 		Var.addCoin("ripple",61.11809925,116.1084172342);
 		Var.addCoin("dogecoin",2000,18.882434);
 		Var.addCoin("reddcoin",1755.52958824,19.062553);
+
+
+		adder = (RelativeLayout)findViewById(R.id.addcard);
+		Button button = (Button)findViewById(R.id.add);
+		button.setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick(View view){
+				adder.setVisibility(View.VISIBLE);
+			}
+		});
+
+		button = (Button)findViewById(R.id.search_back);
+		button.setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick(View view){
+				adder.setVisibility(View.INVISIBLE);
+			}
+		});
 
 	}
 
@@ -80,6 +106,7 @@ public class MainActivity extends AppCompatActivity{
 	public void load(){
 		try{
 			ReadJson read = new ReadJson(openFileInput(Var.FILENAME));
+			read.execute();
 		}catch(Exception e){
 
 		}
