087f85fe35e7f16a11f282acf6b00a78717d0be5
Chris <ChristopherBThai@gmail.com>
2018-01-09 15:07:02 -0800

    Currency list is grabbed from my server

--- app/src/main/java/com/bitprofit/mono/bitprofit/async/FetchCurrencies.java
+++ app/src/main/java/com/bitprofit/mono/bitprofit/async/FetchCurrencies.java
@@ -1,9 +1,17 @@
 package com.bitprofit.mono.bitprofit.async;
 
+import android.content.res.AssetManager;
 import android.os.AsyncTask;
 
 import com.bitprofit.mono.bitprofit.MainActivity;
 import com.bitprofit.mono.bitprofit.variables.Var;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Reads list of available currencies
@@ -11,13 +19,33 @@ import com.bitprofit.mono.bitprofit.variables.Var;
  */
 
 public class FetchCurrencies extends AsyncTask<Void,Void,Void>{
+
 	@Override
 	protected Void doInBackground(Void... voids){
-		Var.addAvailableCoin("Bitcoin","BTC");
-		Var.addAvailableCoin("Litecoin","LTC");
-		Var.addAvailableCoin("Ripple","XRP");
-		Var.addAvailableCoin("Dogecoin","DOGE");
-		Var.addAvailableCoin("Tron","TRON");
+		BufferedReader reader = null;
+		try{
+			URL url = new URL("http://corg.network:8080/bitprofit/list");
+			HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+			InputStream inputStream = httpURLConnection.getInputStream();
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+			String line;
+			while((line=bufferedReader.readLine())!=null){
+				String[] words = line.split("-");
+				Var.addAvailableCoin(words[0],words[1]);
+			}
+
+		}catch(IOException e){
+			e.printStackTrace();
+		}finally{
+			if(reader!=null){
+				try{
+					reader.close();
+				}catch(IOException e){
+					e.printStackTrace();
+				}
+			}
+		}
 		return null;
 	}
 
